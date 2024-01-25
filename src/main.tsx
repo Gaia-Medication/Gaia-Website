781ec2854abe296283cd6d10680a1fcbd6352c9b
E214194U <alexandreclenet@free.fr>
2024-01-25 17:48:45 +0100

    up

--- src/main.tsx
+++ src/main.tsx
@@ -3,26 +3,11 @@ import ReactDOM from 'react-dom/client'
 import Home from './pages/Home.tsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Contact from './pages/Contact.tsx'
-import Team from './pages/Team.tsx'
-import Application from './pages/Application.tsx'
 
 const router = createBrowserRouter([
   {
     path: "Gaia-Website/",
     element: <Home/>,
-  },
-  {
-    path: "Gaia-Website/Contact/",
-    element: <Contact/>,
-  },
-  {
-    path: "Gaia-Website/Application/",
-    element: <Application/>,
-  },
-  {
-    path: "Gaia-Website/Team/",
-    element: <Team/>,
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
