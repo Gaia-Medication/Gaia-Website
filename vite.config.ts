65438a3a60bc2f024b9665d453185a6573759696
florian <florian.tranpro@gmail.com>
2024-01-29 23:33:55 +0100

    add: three and fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base:"/",
+  base: '/',
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src'
+    },
+  },
+});
