e131e76be9b97fc03c9ac50f4c6051a12e8ecd9a
Alexandre Clenet <alexandreclenet@free.fr>
2024-01-29 10:21:23 +0100

    pdf

--- src/componant/Nav.tsx
+++ src/componant/Nav.tsx
@@ -11,10 +11,10 @@ function Nav() {
   return (
       <nav className="flex flex-row justify-around h-20 items-center bg-white">
         <div className="ml-20">
-          <img src="/src/assets/logo_title_gaia.png" className=" h-14" alt="Gaïa" />
+          <img src="src/assets/logo_title_gaia.png" className=" h-14" alt="Gaïa" />
         </div>
         <div className="flex flex-row gap-12 items-center mr-10">
-          <Link to={`/`} className={`text-lg font-bold ${location.pathname === '/' ? 'text-[#9CDE00]' : ''}`} >Accueil</Link>
+          <Link to={`/Gaia-Website/`} className={`text-lg font-bold ${location.pathname === '/' ? 'text-[#9CDE00]' : ''}`} >Accueil</Link>
           <Link to={`/Application/`} className={`text-lg font-bold ${location.pathname === '/Application/' ? 'text-[#9CDE00]' : ''}`}>Application</Link>
           <Link to={`/Team/`} className={`text-lg font-bold ${location.pathname === '/Team/' ? 'text-[#9CDE00]' : ''}`}>Équipe</Link>
           <Link to={`/Contact/`} className={`text-lg font-bold ${location.pathname === '/Contact/' ? 'text-[#9CDE00]' : ''}`}>Contact</Link>
