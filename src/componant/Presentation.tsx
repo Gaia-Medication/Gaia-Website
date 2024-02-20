b4dff3d2cd10dd328182b40b7ff9fd74822efd53
florian <florian.tranpro@gmail.com>
2024-02-20 14:35:58 +0100

    fix: presentation responsive

--- src/componant/Presentation.tsx
+++ src/componant/Presentation.tsx
@@ -19,40 +19,40 @@ const Presentation: React.FC = () => {
       </div>
 
       <div className="flex md:flex-row flex-col flex-wrap items-center justify-center mt-8 w-[90%]">
-        <div className="flex flex-col items-center gap-2 bg-white rounded-lg md:shadow-2xl shadow-neutral-200 p-6 pt-10 mx-6 md:w-[25%] w-[80%] max-h-[500px] xl:min-h-[500px] duration-200 md:hover:shadow-indigo-100">
+        <div className="flex flex-col items-center gap-2 bg-white rounded-lg md:shadow-2xl shadow-neutral-200 p-6 pt-10 mx-0 sm:mx-6 md:w-[25%] w-full sm:w-[80%] max-h-[300px] sm:max-h-[500px] xl:min-h-[500px] duration-200 md:hover:shadow-indigo-100 overflow-auto">
           <div className="flex items-center justify-center h-16 w-16 bg-indigo-200 rounded-full"><img src={stockIcon} alt="stock icon" className="h-[85%]"></img></div>
-          <h3 className=" text-xl lg:text-3xl font-normal text-gray-700 mt-4 tracking-wide text-center">
+          <h3 className=" text-base sm:text-xl lg:text-3xl font-normal text-gray-700 mt-4 tracking-wide text-center">
             Suivi de traitement & de stock
           </h3>
-          <div className="w-[80%]">
-            <p className="hidden justify-center items-center text-gray-400 mt-2 text-center text-sm max-[768px]:flex max-[768px]:text-xs xl:flex">
+          <div className="w-full sm:w-[80%]">
+            <p className="hidden justify-center items-center text-gray-400 mt-2 text-center text-xs sm:text-sm max-[768px]:flex max-[768px]:text-xs xl:flex">
               Gaïa vous notifie pour chaque médicament à prendre et suit votre stock, garantissant aucun oubli et une gestion simplifiée de vos médicaments.
             </p>
           </div>
           <button className="bg-white hover:bg-gray-100 text-gray-600 font-medium py-2 px-4 rounded-full mt-auto border text-sm">Plus de détails</button>
         </div>
 
-        <div className="flex flex-col items-center gap-2 bg-white rounded-lg md:shadow-2xl shadow-neutral-200 p-6 pt-10 mx-6 md:w-[25%] w-[80%] max-h-[500px] xl:min-h-[500px] duration-200 md:hover:shadow-yellow-100">
+        <div className="flex flex-col items-center gap-2 bg-white rounded-lg md:shadow-2xl shadow-neutral-200 p-6 pt-10 mx-0 sm:mx-6 md:w-[25%] w-full sm:w-[80%] max-h-[300px] sm:max-h-[500px] xl:min-h-[500px] duration-200 md:hover:shadow-yellow-100 overflow-auto">
           <div className="flex items-center justify-center h-16 w-16 bg-yellow-200 rounded-full"><img src={mapIcon} alt="map icon"></img></div>
-          <h3 className=" text-xl lg:text-3xl font-normal text-gray-700 mt-4 tracking-wide text-center">
+          <h3 className=" text-base sm:text-xl lg:text-3xl font-normal text-gray-700 mt-4 tracking-wide text-center">
             Carte des établissements de santé
           </h3>
-          <div className="w-[80%]">
-            <p className="hidden justify-center items-center text-gray-400 mt-2 text-center text-sm max-[768px]:flex max-[768px]:text-xs xl:flex">
+          <div className="w-full sm:w-[80%]">
+            <p className="hidden justify-center items-center text-gray-400 mt-2 text-center text-xs sm:text-sm max-[768px]:flex max-[768px]:text-xs xl:flex">
               Gaia localise les établissements de santé à proximité, vous assurant de toujours disposer de vos médicaments lors de vos traitements
             </p>
           </div>
           <button className="bg-white hover:bg-gray-100 text-gray-600 font-medium py-2 px-4 rounded-full mt-auto border text-sm">Plus de détails</button>
         </div>
 
-        <div className="flex flex-col items-center gap-2 bg-white rounded-lg md:shadow-2xl shadow-neutral-200 p-6 pt-10 mx-6 md:w-[25%] w-[80%] max-h-[500px] xl:min-h-[500px] duration-200 md:hover:shadow-cyan-100">
+        <div className="flex flex-col items-center gap-2 bg-white rounded-lg md:shadow-2xl shadow-neutral-200 p-6 pt-10 mx-0 sm:mx-6 md:w-[25%] w-full sm:w-[80%] max-h-[300px] sm:max-h-[500px] xl:min-h-[500px] duration-200 md:hover:shadow-cyan-100 overflow-auto">
           <div className="flex items-center justify-center h-16 w-16 bg-cyan-100 rounded-full"><img src={researchMed} alt="composition icon"></img></div>
 
-          <h3 className=" text-xl lg:text-3xl font-normal text-gray-700 mt-4 tracking-wide text-center">
+          <h3 className=" text-base sm:text-xl lg:text-3xl font-normal text-gray-700 mt-4 tracking-wide text-center">
             Scan & recherche de produits médicaux
           </h3>
-          <div className="w-[80%]">
-            <p className="hidden justify-center items-center text-gray-400 mt-2 text-center text-sm max-[768px]:flex max-[768px]:text-xs xl:flex">
+          <div className="w-full sm:w-[80%]">
+            <p className="hidden justify-center items-center text-gray-400 mt-2 text-center text-xs sm:text-sm max-[768px]:flex max-[768px]:text-xs xl:flex">
               Gaia offre un accès rapide aux informations en scannant la boîte, et la recherche vous aide à trouver rapidement un médicament spécifique. Scanner une ordonnance ajoute directement votre traitement dans l'application
             </p>
           </div>
