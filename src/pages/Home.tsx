936d506f895728c454b5a79b5740cf21f00612d1
florian <florian.tranpro@gmail.com>
2024-02-21 09:24:49 +0100

    add: meta description

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import Team from "../componant/Team";
 function Home() {
   return (
     <div className=" min-w-[360px]">
+      <meta name="description" content="Gaia Medication : votre application gratuite de santé pour une gestion complète des traitements, rappels de médicaments, et suivi des stocks. Inclut la gestion des effets secondaires, une carte interactive des établissements de santé et médecins, ainsi qu'une communication facilitée avec les professionnels de santé par envoi de résumés PDF. Simplifiez votre suivi médical et gardez le contrôle de votre bien-être."></meta>
       <Header></Header>
       <Showcase></Showcase>
       <Presentation></Presentation>
