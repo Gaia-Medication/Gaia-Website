46767dedd9c391b7891fa7282c94d648076e0a49
florian <florian.tranpro@gmail.com>
2024-02-22 10:42:14 +0100

    add style: contact

--- src/componant/Contact.tsx
+++ src/componant/Contact.tsx
@@ -1,7 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { useState } from "react";
 
-
 const Contact = () => {
     const apiKey = import.meta.env.VITE_BREVO_API_KEY;
 
@@ -26,7 +25,7 @@ const Contact = () => {
             ...formData,
             [e.target.name]: e.target.value,
         });
-    }
+    };
 
     const sendEmail = async () => {
         console.log(formData);
@@ -44,7 +43,7 @@ const Contact = () => {
                             name: formData.recipientName,
                         },
                     ],
-                    subject: formData.subject,
+                    subject: `Contact du site web Gaia-Medication de: ${formData.senderName}`,
                     htmlContent: formData.htmlContent,
                 },
                 {
@@ -69,57 +68,51 @@ const Contact = () => {
                 console.log(e.request);
             } else {
                 // Quelque chose s'est mal passé lors de la mise en place de la requête
-                console.log('Error', e.message);
+                console.log("Error", e.message);
             }
         }
     };
 
     return (
-        <div>
-            <form>
-                <label>
-                    Email de l'expéditeur :
-                    <input
-                        type="email"
-                        name="senderEmail"
-                        value={formData.senderEmail}
-                        onChange={handleChange}
-                    />
-                </label>
-                <br />
-                <label>
-                    Nom de l'expéditeur :
-                    <input
-                        type="text"
-                        name="senderName"
-                        value={formData.senderName}
-                        onChange={handleChange}
-                    />
-                </label>
-                <br />
-                <label>
-                    Sujet :
-                    <input
-                        type="text"
-                        name="subject"
-                        value={formData.subject}
-                        onChange={handleChange}
-                    />
-                </label>
-                <br />
-                <label>
-                    Contenu HTML :
-                    <textarea
-                        name="htmlContent"
-                        value={formData.htmlContent}
-                        onChange={handleContent}
-                    />
-                </label>
-                <br />
-                <button type="button" onClick={sendEmail}>
-                    Envoyer l'email
-                </button>
-            </form>
+        <div className="flex flex-col justify-center items-center h-[400px]">
+            <p className="text-base sm:text-xl text-lime-500 font-semibold">Contactez-nous</p>
+            <h2 className="text-xl sm:text-4xl text-gray-800 font-semibold">Contact</h2>
+            <div className="h-full w-[60%]">
+                <form className="grid grid-cols-2 gap-4">
+                    <div>
+                        <input
+                            placeholder="Email"
+                            type="email"
+                            name="senderEmail"
+                            value={formData.senderEmail}
+                            onChange={handleChange}
+                            className="border-b m-4 p-2"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            placeholder="Nom"
+                            type="text"
+                            name="senderName"
+                            value={formData.senderName}
+                            onChange={handleChange}
+                            className="border-b m-4 p-2"
+                        />
+                    </div>
+                    <div className=" col-span-full ">
+                        <textarea
+                            placeholder="Votre message"
+                            name="htmlContent"
+                            value={formData.htmlContent}
+                            onChange={handleContent}
+                            className="border m-4 w-full h-full p-4"
+                        />
+                    </div>
+                    <button type="button" onClick={sendEmail}>
+                        Envoyer l'email
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
