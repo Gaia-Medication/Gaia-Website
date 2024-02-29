69caa47cd9c00bfe720940aa72b886000a2be47f
florian <florian.tranpro@gmail.com>
2024-02-29 03:17:03 +0100

    add: animation

--- src/componant/Showcase.tsx
+++ src/componant/Showcase.tsx
@@ -1,10 +1,21 @@
 import gaiaPhone from "../assets/3d-gaia-phone.png";
 import { ArrowRight } from "react-feather";
+import { useEffect, useState } from "react";
 
 const Showcase = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
   return (
-    <div id="home" className="flex sm:mt-40 mt-0 justify-center items-center p-2 xl:h-[78vh] md:h-[60vh] h-[50vh]">
-      <div className="flex flex-col justify-center w-[55%] h-full pl-[6%] sm:pl-[12%] gap-12">
+    <div
+      id="home"
+      className={`flex sm:mt-40 mt-0 justify-center items-center p-2 xl:h-[78vh] md:h-[60vh] h-[50vh]`}
+    >
+      <div className={`flex flex-col justify-center w-[55%] h-full pl-[6%] sm:pl-[12%] gap-12 ${isLoaded ? "opacity-100" : "opacity-0"
+          } transition-opacity duration-[3000ms]`}>
         <div className="flex flex-col w-full gap-2">
           <div className="bg-blue-400 rounded-full px-2 max-w-12 lg:ml-6 transition-all duration-500">
             <p className="text-white font-semibold">New</p>
@@ -34,8 +45,10 @@ const Showcase = () => {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center w-[45%] md:h-full sm:h-[50vh] h-[35vh] relative">
-        <img src={gaiaPhone} alt="Gaia Phone" className="w-[80%] xl:w-[60%] h-auto max-w-full z-10 transition-all duration-500" />
-        <div className="bg-lime-400 rounded-full xl:w-[550px] xl:h-[550px] md:w-[380px] md:h-[380px] sm:w-[250px] sm:h-[250px] w-[150px] h-[150px] absolute transition-all duration-500"></div>
+        <img src={gaiaPhone} alt="Gaia Phone" className={`w-[80%] xl:w-[60%] h-auto max-w-full z-10 transition-all ${isLoaded ? "opacity-100" : "opacity-0"
+          } transition-opacity duration-[1000ms]`} />
+        <div className={`bg-lime-400 rounded-full xl:w-[550px] xl:h-[550px] md:w-[380px] md:h-[380px] sm:w-[250px] sm:h-[250px] w-[150px] h-[150px] absolute transition-all ${isLoaded ? "opacity-100" : "opacity-0"
+          } transition-opacity duration-[2000ms] `}></div>
       </div>
     </div>
   );
